fa3654a37aa2b5409f8c0f695413cd63770a0a90
miguel11065 <miguel11065@gmail.com>
2024-02-24 21:23:27 -0300

    publicando meu jogo

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,24 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(evento) {
+    chute = evento.results[0][0].transcript
+    exibeChuteNaTele(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
+}
+
+function exibeChuteNaTele(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
